71f81327570b57fd6047e2bf36dd83d9d2201739
Willi Krappen <stackola@arch-tower.lan>
2018-01-19 21:28:03 +0100

    add prod task

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,85 @@
-var gulp = require("gulp");
-var less = require("gulp-less");
-var webpack = require("webpack-stream");
-var ts = require("gulp-typescript");
-var browserSync = require("browser-sync").create();
+var gulp = require('gulp');
+var less = require('gulp-less');
+var webpack = require('webpack-stream');
+var webpack2 = require('webpack');
+var ts = require('gulp-typescript');
+var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
-var tsProject = ts.createProject("tsconfig.json");
+var tsProject = ts.createProject('tsconfig.json');
 
 /* Task to compile less */
-gulp.task("compile-less", function() {
+gulp.task('compile-less', function() {
   gulp
-    .src("./src/less/main.less")
+    .src('./src/less/main.less')
     .pipe(less())
-    .pipe(gulp.dest("./dist/css/"));
+    .pipe(gulp.dest('./dist/css/'));
 });
 /* Task to watch less changes */
-gulp.task("watch-less", function() {
-  gulp.watch("./src/less/**/*.less", ["compile-less"]);
+gulp.task('watch-less', function() {
+  gulp.watch('./src/less/**/*.less', ['compile-less']);
 });
 
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
 
-gulp.task("webpack", function() {
+var webpackConf = {
+  watch: true,
+  entry: ['whatwg-fetch', 'babel-polyfill', './src/ts/main.ts'],
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+gulp.task('prod', function() {
   return gulp
-    .src("./dist/js/main.js")
+    .src('./dist/js/main.js')
     .pipe(
       webpack({
-        watch: true,
-        entry: ["whatwg-fetch", "babel-polyfill", "./src/ts/main.ts"],
+        ...webpackConf,
         output: {
-          filename: "bundle.js"
+          filename: 'prod.js',
         },
-        module: {
-          rules: [
-            {
-              test: /\.tsx?$/,
-              use: "ts-loader",
-              exclude: /node_modules/
-            }
-          ]
-        },
-        resolve: {
-          extensions: [".tsx", ".ts", ".js"]
-        }
+        plugins: [
+          new webpack2.DefinePlugin({
+            'process.env': { NODE_ENV: "'production'" },
+          }),
+          new webpack2.optimize.UglifyJsPlugin(),
+        ],
       })
     )
-    .pipe(gulp.dest("dist/js"));
+    .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task("serve", function() {
+gulp.task('webpack', function() {
+  return gulp
+    .src('./dist/js/main.js')
+    .pipe(webpack(webpackConf))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('serve', function() {
   // Serve files from the root of this project
   browserSync.init({
     server: {
-      baseDir: "./dist/"
-    }
+      baseDir: './dist/',
+    },
   });
-  gulp.watch("./dist/**/*.css").on("change", reload);
-  gulp.watch("./dist/**/*.html").on("change", reload);
-  gulp.watch("./dist/**/*.js").on("change", reload);
+  gulp.watch('./dist/**/*.css').on('change', reload);
+  gulp.watch('./dist/**/*.html').on('change', reload);
+  gulp.watch('./dist/**/*.js').on('change', reload);
 });
 
 /* Task when running `gulp` from terminal */
-gulp.task("default", ["watch-less", "serve", "webpack"]);
+gulp.task('default', ['watch-less', 'serve', 'webpack']);
